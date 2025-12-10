4e58f083e8e3e296b5888c448d92109e3d941fa6
David Lexe <David_Lexe@hotmail.com>
2025-12-10 16:09:49 +0100

    Foundation
    Foundations for maven implementation (aka pom.xml) has been established.

--- Backend/src/Main.java
+++ Backend/src/Main.java
@@ -1,6 +1,14 @@
-public class Main {
+import io.javalin.Javalin;
+import io.javalin.plugin.bundled.CorsPluginConfig;
 
-public static void main(String[] args) {
-    System.out.println("Hello world");
+public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create(config -> {
+                    config.plugins.enableCors(cors -> {
+                        cors.add(CorsPluginConfig::anyHost);
+                    });
+                })
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
 }
-        }
